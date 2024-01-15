d64c38519ff6f9f8845a4441efa567843b6aa57e
Sayantan <sayantann2002@gmail.com>
2024-01-16 00:01:12 +0530

    Basic frontend set

---
+++ web/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Auth } from "@/components/blocks/auth";
+
+export default function Home() {
+  return (
+    <main className="flex flex-row">
+      <section className="hidden md:block md:w-1/3 bg-gray-400 text-center p-40"></section>
+      <section className="w-full md:w-2/3 content-center flex min-h-screen flex-col items-center justify-between p-24">
+        <Auth />
+      </section>
+    </main>
+  );
+}
